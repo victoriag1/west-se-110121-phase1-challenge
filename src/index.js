3a3293f579534418f9b442beeef38f81baefdd88
Victoria Gonzalez <gonzalezevic@gmail.com>
2021-11-12 11:01:08 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// write your code here
+const imagesAPI = 'http://localhost:3000/images/1'
+const title = "Woofing those bugs away"
+
+fetch(imagesAPI)
+.then((res) => res.json())
+.then((json) => console.log(json))
+
+function 
+imagesAPI.title = title
+image.src= 
